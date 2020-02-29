ae27cc1e59bde44ddc9f2a55cd216b490d5802ac
Niko Kaartinen <niko.kaartinen@protonmail.com>
2020-02-29 16:47:59 +0200

    Increase rate limit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ db.initDatabase();
 const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 2, // limit each IP to 100 requests per windowMs
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 app.set('trust proxy', 1);
 
